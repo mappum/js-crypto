219b9253bb6183219588b7b6e1e4010150d9022b
Matt Bell <mappum@gmail.com>
2017-01-13 11:39:09 -0800

    Added symmetric encryption to JS wrapper

--- src/wrapper.js
+++ src/wrapper.js
@@ -6,7 +6,7 @@ exports.armor = {
   },
   decode (armor) {
     let [ type, headers, data, err ] = crypto.DecodeArmor(armor)
-    if (err) throw new Error(err)
+    if (err != null) throw new Error(err)
     return { type, headers, data }
   }
 }
@@ -16,3 +16,14 @@ exports.ripemd160 = crypto.Ripemd160
 
 exports.PrivKey = require('./privkey.js')
 exports.PubKey = require('./pubkey.js')
+
+exports.symmetric = {
+  encrypt (plaintext, secret) {
+    return crypto.EncryptSymmetric(plaintext, secret)
+  },
+  decrypt (ciphertext, secret) {
+    let [ plaintext, err ] = crypto.DecryptSymmetric(ciphertext, secret)
+    if (err != null) throw new Error(err)
+    return plaintext
+  }
+}
