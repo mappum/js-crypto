17d8348ae0a96308407b3b55429cab84c8fbabf4
Matt Bell <mappum@gmail.com>
2017-01-18 19:59:17 -0800

    Added Signature type

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = Object.assign({
   PrivKey: require('./lib/privkey.js'),
   PubKey: require('./lib/pubkey.js'),
+  Signature: require('./lib/signature.js'),
   keyAlgorithms: require('./lib/keyAlgorithms'),
   varint: require('./lib/varint.js')
 }, require('./lib/hash.js'))
---
+++ src/signature.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const algorithms = require('./keyAlgorithms')
+
+function decode (buffer, start = 0, end = buffer.length) {
+  let algo = algorithms.get(buffer[start])
+  let length = algo.sigLength + 1
+  let signature = buffer.slice(start + 1, start + length)
+  decode.bytes = length
+  return signature
+}
+
+// TODO: include length prefix for secp256k1 since it is
+// defined as a variable-length in go-crypto
+function encode (sig, buffer, offset = 0) {
+  let length = encodingLength(sig)
+  buffer = buffer || Buffer.alloc(length)
+  buffer[offset] = sig.type
+  sig.copy(buffer, offset + 1)
+  encode.bytes = length
+  return buffer
+}
+
+function encodingLength (sig) {
+  return sig.length + 1
+}
+
+module.exports = {
+  decode,
+  encode,
+  encodingLength
+}
