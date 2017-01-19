47822a8cc38ca2147d2f7156494cc9d708af7ae7
Matt Bell <mappum@gmail.com>
2017-01-18 18:23:21 -0800

    Export key algorithms

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = Object.assign({
   PrivKey: require('./lib/privkey.js'),
-  PubKey: require('./lib/pubkey.js')
+  PubKey: require('./lib/pubkey.js'),
+  keyAlgorithms: require('./lib/keyAlgorithms')
 }, require('./lib/hash.js'))
-
